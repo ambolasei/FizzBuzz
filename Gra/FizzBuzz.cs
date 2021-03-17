6c312434cb9105a4858eb29c5315fc4028f5dd80
ambolasei <ambolasei@gmail.com>
2021-03-17 21:23:39 +0100

    first commit

---
+++ Gra/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gra
+{
+    class FizzBuzz
+    {
+        public string Play(int Number)
+        {
+            if (Number%3==0 && Number%5!=0)
+            {
+                return "Fizz";
+            }
+            else if (Number%5==0 && Number%3!=0)
+                {
+                return "Buzz";
+            }
+            else if (Number%3==0 && Number%5==0)
+            {
+                return "FizzBuzz";
+            }
+            else
+            {
+                return Convert.ToString(Number);
+            }
+        }
+    }
+}
---
+++ Gra/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj dowolną liczbę: ");
+            var Number = GetNumber();
+
+            var Score = new FizzBuzz();
+            Console.WriteLine(Score.Play(Number));
+        }
+
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if(!int.TryParse(Console.ReadLine(), out int Number))
+                {
+                    Console.WriteLine("Podana liczba jest nieprawidłowa. Spróbuj ponownie: ");
+                    continue;
+                }
+                return Number;
+            }
+        }
+    }
+}
